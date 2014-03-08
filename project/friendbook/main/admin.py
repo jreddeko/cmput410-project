d833602767850e60aee103ebcae9a204339a0b25
Ga Young Kim <gayoung1@cs.ualberta.ca>
2014-03-08 10:31:23 -0700

    trying commit

--- project/friendbook/main/admin.py
+++ project/friendbook/main/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+
+from main.models import Users, Posts, Comment, Friends
+
+admin.site.register(Users)
+admin.site.register(Posts)
+admin.site.register(Comment)
+admin.site.register(Friends)
