bab5ee60c914b499da73eb8201e6a4ba90ace632
Ga Young Kim <gayoung1@ualberta.ca>
2014-03-09 15:13:10 -0600

    split create post and post wall but currently having erros with tinymce

--- project/friendbook/static/js/postwall.js
+++ project/friendbook/static/js/postwall.js
@@ -94,63 +94,63 @@ $(document).ready(function (){
     });
 
 });
-
-function processPostData()
-{
-    var permission = $("#post_permissions").val();
-    var authorname = $("#spec_author_input").val();
-    var title = $("#post_title").val();
-    var description = $("#post_description").val();
-    var content = tinymce.get("post_content").getContent();
-    var host = window.location.host;
-    var posttime = $.now();
-    
-    //var source = not done yet
-    //var origin = not done yet
-    //var category = not implemented yet
-    //var guid = ??
-    
-    // make the JSON string and pass to AJAX and call url
-    // not tested yet!
-    var targetUrl = $("#new_post").attr("action");
-    $.ajax({
-        url: targetUrl,
-        type: 'PUT',
-        contentType: 'text/html',
-        data:{
-            "posts":[
-                {
-                    "title": title,
-                    "source": 'blah',
-                    "origin": 'blah',
-                    "description": description,
-                    "content-type": 'text/html',
-                    "content": content,
-                    "author": {
-                        "id": 1,
-                        "host": host,
-                        "displayname": "gayoung",
-                        "url": host+"author/gayoung"
-                    },
-                    "categories": "['hello', 'world']",
-                    "comments": [{}],
-                    "pubDate": posttime,
-                    "guid": 'blah',
-                    "visibility": permission
-                    
-                }
-            ]
-        },
-        success: function() {
-            alert("success");
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            alert ("AJAX error: "+ jqXHR + ", "+textStatus+", "+errorThrown);
-        }
-    })
-    
-    
-}
+//
+//function processPostData()
+//{
+//    var permission = $("#post_permissions").val();
+//    var authorname = $("#spec_author_input").val();
+//    var title = $("#post_title").val();
+//    var description = $("#post_description").val();
+//    var content = tinymce.get("post_content").getContent();
+//    var host = window.location.host;
+//    var posttime = $.now();
+//    
+//    var source = $("#post_source").val()
+//    //var origin = not done yet
+//    //var category = not implemented yet
+//    //var guid = ??
+//    
+//    // make the JSON string and pass to AJAX and call url
+//    // not tested yet!
+//    var targetUrl = $("#new_post").attr("action");
+//    $.ajax({
+//        url: targetUrl,
+//        type: 'PUT',
+//        contentType: 'text/html',
+//        data:{
+//            "posts":[
+//                {
+//                    "title": title,
+//                    "source": 'blah',
+//                    "origin": 'blah',
+//                    "description": description,
+//                    "content-type": 'text/html',
+//                    "content": content,
+//                    "author": {
+//                        "id": 1,
+//                        "host": host,
+//                        "displayname": "gayoung",
+//                        "url": host+"author/gayoung"
+//                    },
+//                    "categories": "['hello', 'world']",
+//                    "comments": [{}],
+//                    "pubDate": posttime,
+//                    "guid": 'blah',
+//                    "visibility": permission
+//                    
+//                }
+//            ]
+//        },
+//        success: function() {
+//            alert("success");
+//        },
+//        error: function(jqXHR, textStatus, errorThrown) {
+//            alert ("AJAX error: "+ jqXHR + ", "+textStatus+", "+errorThrown);
+//        }
+//    })
+//    
+//    
+//}
 
 function text2form(currentdbId)
 {
@@ -161,7 +161,10 @@ function text2form(currentdbId)
         
         // get currently displayed contents of the post
         var headerContent = $("#post-"+currentdbId+" h2").first().text();
-        var description = $("#post_description-"+currentdbId).text();
+        var sourceContent = $("#post_source-"+currentdbId).find("span").first().text();
+        var originContent = $("#post_origin-"+currentdbId).find("span").first().text();
+        var categoryContent = $("#post_category-"+currentdbId).find("span").first().text();
+        var description = $("#post_description-"+currentdbId).find("p").first().text();
         var bodyContent = '';
         $("#post_content-"+currentdbId).children().each(function() {
             if(this.tagName != "h2")
@@ -175,10 +178,26 @@ function text2form(currentdbId)
         
         $(editForm).insertBefore($("#post-"+currentdbId));
         $("#post-"+currentdbId).css("display", "none");
+    
+        tinymce.init({
+             selector: "#edit_post_content-"+currentdbId,
+             plugins: [
+                       "advlist autolink lists link image charmap preview anchor",
+                       "searchreplace visualblocks code fullscreen",
+                       "insertdatetime media table contextmenu paste"
+                       ],
+             toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+        });
+    
+        tinymce.execCommand('mceRemoveControl', false, "edit_post_content-"+currentdbId);
+        tinymce.execCommand('mceAddEditor', true, "edit_post_content-"+currentdbId);
         
         $("#spec_author_div-"+currentdbId).hide();
         
         $("#post_title-"+currentdbId).val(headerContent);
+        $("#post_source-"+currentdbId).val(sourceContent);
+        $("#post_origin-"+currentdbId).val(originContent);
+        $("#post_category-"+currentdbId).val(categoryContent);
 
         $('select#post_permissions-'+currentdbId).children().each(function() {
             var currentVal = $(this).attr("value");
@@ -196,17 +215,7 @@ function text2form(currentdbId)
         });
         
         $("#post_description-"+currentdbId).val(description);
-        tinymce.init({
-        selector: "#post_content-"+currentdbId,
-        plugins: [
-            "advlist autolink lists link image charmap preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste"
-        ],
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-        });
-        
-        tinymce.get("post_content-"+currentdbId).setContent(bodyContent+" html");
+        tinymce.get("edit_post_content-"+currentdbId).setContent(bodyContent+" html");
         
 }
 
@@ -216,9 +225,9 @@ function createForm(id)
                     '<h3>Edit Post</h3>'+
                     '<form class="form-horizontal" action="" method="POST">'+
                         '<div class="form-group">'+
-                            '<label for="post_permissions" class="col-sm-2 control-label">Permissions:</label>'+
-                            '<div class="col-sm-10">'+
-                                '<select id="post_permissions-'+id+'" name="post_permissions-"'+id+'>'+
+                            '<label for="post_permissions" class="col-sm-3">Permissions:</label>'+
+                            '<div class="col-sm-9">'+
+                                '<select id="post_permissions-'+id+'" name="post_permissions-"'+id+' class="col-sm-9 form-control" style="width: 90%;">'+
                                     '<option value="private" selected="selected">Private</option>'+
                                         '<option value="spec_author">Specify</option>'+
                                         '<option value="friends">Friends</option>'+
@@ -229,32 +238,50 @@ function createForm(id)
                                 '</div>'+
                             '</div>'+
                              '<div id="spec_author_div-'+id+'" class="form-group">'+
-                                    '<label for="spec_author_input-'+id+'" class="col-sm-2"> Author\'s username:</label>'+
-                                    '<div class="col-sm-10">'+
+                                    '<label for="spec_author_input-'+id+'" class="col-sm-3"> Author\'s username:</label>'+
+                                    '<div class="col-sm-9">'+
                                         '<input id="spec_author_input-'+id+'" name="spec_author_input-'+id+'" style="width: 90%;" placeholder="Please input the author\'s username."/>'+
                                     '</div>'+
                                 '</div>'+
                             '<div class="form-group">'+
-                                '<label for="post_title-'+id+'" class="col-sm-2">Title: </label>'+
-                                '<div class="col-sm-10">'+
-                                    '<input type="text" id="post_title-'+id+'" name="post_title'+id+'" style="width: 90%;" placeholder="Title of the post"/>'+
+                                '<label for="post_title-'+id+'" class="col-sm-3">Title: </label>'+
+                                '<div class="col-sm-9">'+
+                                    '<input type="text" id="post_title-'+id+'" name="post_title-'+id+'" style="width: 90%;" placeholder="Title of the post"/>'+
+                                '</div>'+
+                            '</div>'+
+                 '<div class="form-group">'+
+                    '<label for="post_source-'+id+'" class="col-sm-3">Source of the Post: </label>'+
+                    '<div class="col-sm-9">'+
+                        '<input type="text" id="post_source-'+id+'" name="post_source-'+id+'" style="width: 90%;" placeholder="Source URI of the post"/>'+
+                    '</div>'+
+                 '</div>'+
+                 '<div class="form-group">'+
+                    '<label for="post_origin-'+id+'" class="col-sm-3">Origin of the Post: </label>'+
+                    '<div class="col-sm-9">'+
+                        '<input type="text" id="post_origin-'+id+'" name="post_origin-'+id+'" style="width: 90%;" placeholder="Origin of the post (URI)"/>'+
+                    '</div>'+
+                 '</div>'+
+                 '<div class="form-group">'+
+                    '<label for="post_category-'+id+'" class="col-sm-3">Category of the Post: </label>'+
+                    '<div class="col-sm-9">'+
+                        '<input type="text" id="post_category-'+id+'" name="post_category-'+id+'" style="width: 90%;" placeholder="(e.g. Medical, Health)"/>'+
+                    '</div>'+
+                 '</div>'+
+                            '<div class="form-group">'+
+                                '<label for="post_description-'+id+'" class="col-sm-3">Post Description:</label>'+
+                                '<div class="col-sm-9">'+
+                                  '<input id="post_description-'+id+'" name="post_description-'+id+'" style="width: 90%;" placeholder="Description of the post"/>'+
                                 '</div>'+
                             '</div>'+
                             '<div class="form-group">'+
-                                '<label for="post_description-'+id+'" class="col-sm-2">Post Description:</label>'+
-                                '<div class="col-sm-10">'+
-                                  '<input id="post_description-'+id+'" name="post_description-'+id+'" style="width: 90%;" placeholder="Description of the post"/>'+  
+                                '<label for="edit_post_content-'+id+'" class="col-sm-3">Post Content:</label>'+
+                                '<div class="col-sm-9">'+
+                                  '<textarea id="edit_post_content-'+id+'" name="edit_post_content-'+id+'" style="width: 90%;"></textarea>'+
                                 '</div>'+
                             '</div>'+
                             '<div class="form-group">'+
-                                '<label for="post_content-'+id+'" class="col-sm-2">Post Content:</label>'+
-                                '<div class="col-sm-10">'+
-                                  '<textarea id="post_content-'+id+'" name="post_content" style="width: 90%;"></textarea>'+ 
-                                '</div>'+
-                            '</div>'+
-                            '<div class="form-group">'+
-                                '<label class="col-sm-2"></label>'+
-                                '<div class="col-sm-10">'+
+                                '<label class="col-sm-3"></label>'+
+                                '<div class="col-sm-9">'+
                                     '<button class="btn btn-primary btn-md" name="edit" type="submit" width: 10%;">Edit</button>'+
                                     '<button id="edit_cancel" class="btn btn-default btn-md" width: 10%;">Cancel</button>'+
                                 '</div>'+
