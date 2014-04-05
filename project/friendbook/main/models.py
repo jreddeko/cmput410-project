a98db3f6bd6c0450a6c5b7cbde238675e435ff56
caciula <caciula@ualberta.ca>
2014-04-05 15:20:45 -0600

    user ids have been changed to guids

--- project/friendbook/main/models.py
+++ project/friendbook/main/models.py
@@ -10,7 +10,7 @@ PERMISSION_LEVELS = (
 )
 
 class Users(models.Model):
-    #guid = models.IntegerField(primary_key=True)
+    guid = models.CharField(max_length=100, primary_key=True)
     username = models.TextField(unique=True)
     password = models.TextField()
     role = models.TextField()
